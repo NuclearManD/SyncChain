d0ba81d40f01b755cb75f9ea9cea9d2ec8b5b681
NuclearManD <dylanbrophy@gmail.com>
2019-06-26 17:55:29 -0700

    made it a package

---
+++ py/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nuclaer-sync-chain",
+    version="0.0.0",
+    author="Dylan Brophy",
+    author_email="dylanbrophy@gmail.com",
+    description="Blockchain for data storage and management",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NuclearManD/SyncChain",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
